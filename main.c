84620c2d1e9622d0810519354fd1ad8ebd26dd21
Sambuddha Basu <sambuddhabasu1@gmail.com>
2015-02-20 15:41:22 +0530

    Client server basic implementation added

--- main.c
+++ main.c
@@ -18,16 +18,18 @@ struct network_info {
 	struct sockaddr_in server_addr, client_addr;
 } self, other;
 
+char NICK[MAXBUF];
+
 // Function declarations
 void print_error_message(char *message);
 void init_server();
-void init_client(char *server_ip);
+void init_client();
 void handle_server();
-void handle_client(char *server_ip);
+void handle_client();
 
 void print_error_message(char *message) {
 	fprintf(stderr, message);
-	exit(EXIT_FAILURE);
+//	exit(EXIT_FAILURE);
 }
 
 void init_server() {
@@ -69,11 +71,12 @@ void handle_server() {
 	char server_buffer[MAXBUF];
 	socklen_t addr_len;
 	init_server();
+	printf("[%s-SERVER]: Serving at: %s:%d\n", NICK, inet_ntoa(self.server_addr.sin_addr), PORT);
 	// Serve forever
 	while(1) {
 		addr_len = sizeof(other.client_fd);
 		other.client_fd = accept(self.server_fd, (struct sockaddr*)&other.client_addr, &addr_len);
-		printf("%s:%d connected\n", inet_ntoa(other.client_addr.sin_addr), ntohs(other.client_addr.sin_port));
+		printf("[%s-SERVER]: Client %s:%d connected\n", NICK, inet_ntoa(other.client_addr.sin_addr), ntohs(other.client_addr.sin_port));
 		/*---Echo back anything sent---*/
 		send(other.client_fd, server_buffer, recv(other.client_fd, server_buffer, MAXBUF, 0), 0);
 		/*---Close data connection---*/
@@ -83,50 +86,52 @@ void handle_server() {
 	return;
 }
 
-void init_client(char *server_ip) {
-	char error_message[MAXBUF];
+void init_client() {
+	int connected = 0, valid_server_ip = 0;
+	char error_message[MAXBUF], server_ip[MAXBUF];
 	// Initialize IP address / port structure
 	bzero(&self.client_addr, sizeof(self.client_addr));
 	self.client_addr.sin_family = AF_INET;
 	self.client_addr.sin_port = htons(PORT);
-	printf("%s\n", server_ip);
-	if(inet_aton(server_ip, &self.client_addr.sin_addr) == 0) {
-		print_error_message(server_ip);
-	}
 
 	// Create the socket
 	if((self.client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		sprintf(error_message, "Error creating socket: %s\n", strerror(errno));
 		print_error_message(error_message);
 	}
+
+	while(!valid_server_ip || !connected) {
+		printf("Enter server IP: ");
+		scanf("%s", server_ip);
+		if(inet_aton(server_ip, &self.client_addr.sin_addr) == 0) {
+			valid_server_ip = 0;
+			printf("Error: Not a valid IP\n");
+		}
+		else {
+			valid_server_ip = 1;
+			if(connect(self.client_fd, (struct sockaddr*)&self.client_addr, sizeof(self.client_addr)) == 0) {
+				connected = 1;
+			}
+			else {
+				connected = 0;
+				printf("Error: Server seems to be down\n");
+			}
+		}
+	}
 }
 
-void handle_client(char *server_ip) {
-	init_client(server_ip);
+void handle_client() {
+	char client_buffer[MAXBUF];
+	init_client();
+	printf("[%s-CLIENT]: Connected to server: %s:%d\n", NICK, inet_ntoa(self.client_addr.sin_addr), PORT);
 	close(self.client_fd);
 }
 
 int main(int argc, char *argv[]) {
 	int status, option;
 	pid_t pid;
-	char *server_ip = NULL;
-	while((option = getopt (argc, argv, "s:")) != -1) {
-		switch(option) {
-			case 's':
-				server_ip = optarg;
-				break;
-			case '?':
-				if(optopt == 's')
-					fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-				else if(isprint(optopt))
-					fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-				else
-					fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
-				return 1;
-			default:
-				exit(EXIT_FAILURE);
-		}
-	}
+	printf("Enter nick: ");
+	scanf("%s", NICK);
 	if((pid = fork()) < 0) {
 		print_error_message("Failed to fork process\n");
 	}
@@ -134,7 +139,7 @@ int main(int argc, char *argv[]) {
 		handle_server();
 	}
 	else {
-		handle_client(server_ip);
+		handle_client();
 	}
 	wait(&status);
 	return 0;
