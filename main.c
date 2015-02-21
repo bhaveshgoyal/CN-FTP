f19ae456957e68f53414384e152a520a9d8813e0
Sambuddha Basu <sambuddhabasu1@gmail.com>
2015-02-21 12:21:47 +0530

    Code reads better now

--- main.c
+++ main.c
@@ -15,24 +15,19 @@
 #define BACKLOG		20
 
 struct network_info {
-	int server_fd, client_fd;
-	struct sockaddr_in server_addr, client_addr;
-} self, other;
-
-struct machine_info {
 	int socket_desc;
 	struct sockaddr_in conn_addr;
-} machine_info;
+} server, in_client, out_client;
 
 char NICK[MAXBUF];
 
 // Function declarations
 void print_error_message(char *message);
 void init_server();
-void init_client();
+void init_out_client();
 void handle_server();
-void handle_client();
-void incoming_connection_handler(void *);
+void handle_out_client();
+void handle_in_client(void *);
 
 void print_error_message(char *message) {
 	fprintf(stderr, message);
@@ -44,31 +39,31 @@ void init_server() {
 	char error_message[MAXBUF];
 
 	// Initialize IP address / port structure
-	bzero(&self.server_addr, sizeof(self.server_addr));
-	self.server_addr.sin_family = AF_INET;
-	self.server_addr.sin_port = htons(PORT);
-	self.server_addr.sin_addr.s_addr = INADDR_ANY;
+	bzero(&server.conn_addr, sizeof(struct sockaddr_in));
+	server.conn_addr.sin_family = AF_INET;
+	server.conn_addr.sin_port = htons(PORT);
+	server.conn_addr.sin_addr.s_addr = INADDR_ANY;
 
 	// Create the socket
-	if((self.server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	if((server.socket_desc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		sprintf(error_message, "Error creating socket: %s\n", strerror(errno));
 		print_error_message(error_message);
 	}
 
 	// Reuse local address
-	if(setsockopt(self.server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+	if(setsockopt(server.socket_desc, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
 		sprintf(error_message, "Error in local address reuse: %s\n", strerror(errno));
 		print_error_message(error_message);
 	}
 
 	// Bind the socket
-	if(bind(self.server_fd, (struct sockaddr*)&self.server_addr, sizeof(self.server_addr)) != 0) {
+	if(bind(server.socket_desc, (struct sockaddr*)&server.conn_addr, sizeof(server.conn_addr)) != 0) {
 		sprintf(error_message, "Error binding socket: %s\n", strerror(errno));
 		print_error_message(error_message);
 	}
 
 	// Make it a listening socket
-	if(listen(self.server_fd, BACKLOG) != 0) {
+	if(listen(server.socket_desc, BACKLOG) != 0) {
 		sprintf(error_message, "Error in listening socket: %s\n", strerror(errno));
 		print_error_message(error_message);
 	}
@@ -79,57 +74,54 @@ void handle_server() {
 	socklen_t addr_len;
 	pthread_t conn_thread;
 	init_server();
-	printf("[%s-SERVER]: Serving at: %s:%d\n", NICK, inet_ntoa(self.server_addr.sin_addr), PORT);
-	addr_len = sizeof(other.client_fd);
+	printf("[%s-SERVER]: Serving at: %s:%d\n", NICK, inet_ntoa(server.conn_addr.sin_addr), PORT);
+	addr_len = sizeof(struct sockaddr_in);
+	FILE *in_clients = fopen("incoming_clients", "w");
 	// Serve forever
 	while(1) {
-		other.client_fd = accept(self.server_fd, (struct sockaddr*)&other.client_addr, &addr_len);
-		machine_info.socket_desc = other.client_fd;
-		machine_info.conn_addr = other.client_addr;
-		pthread_create(&conn_thread, NULL, incoming_connection_handler, (void *)&machine_info);
+		in_client.socket_desc = accept(server.socket_desc, (struct sockaddr*)&in_client.conn_addr, &addr_len);
+		fprintf(in_clients, "%s:%d\n", inet_ntoa(in_client.conn_addr.sin_addr), ntohs(in_client.conn_addr.sin_port));
+		fflush(in_clients);
+		pthread_create(&conn_thread, NULL, handle_in_client, (void *)&in_client);
 	}
 	return;
 }
 
-void incoming_connection_handler(void *socket_info) {
-	//Get the socket descriptor
-	struct machine_info sock = *(struct machine_info *)socket_info;
+void handle_in_client(void *socket_info) {
+	// Get the socket descriptor
+	struct network_info sock = *(struct network_info *)socket_info;
 	int read_size;
 	char *message , client_message[MAXBUF];
 	printf("[%s-SERVER]: Client %s:%d connected\n", NICK, inet_ntoa(sock.conn_addr.sin_addr), ntohs(sock.conn_addr.sin_port));
-	//Send some messages to the client
+
+	// Send some messages to the client
 	message = "Greetings! I am your connection handler\n";
 	write(sock.socket_desc, message, strlen(message));
 	message = "Now type something and i shall repeat what you type \n";
 	write(sock.socket_desc, message, strlen(message));
-	//Receive a message from client
+
+	// Receive a message from client
 	while((read_size = recv(sock.socket_desc, client_message, MAXBUF, 0)) > 0) {
-		//end of string marker
+		// End of string marker
 		client_message[read_size] = '\0';
-		//Send the message back to client
+		// Send the message back to client
 		write(sock.socket_desc, client_message, strlen(client_message));
-		//clear the message buffer
+		// Clear the message buffer
 		memset(client_message, 0, MAXBUF);
 	}
-	if(read_size == 0) {
-		printf("[%s-SERVER]: Client %s:%d disconnected\n", NICK, inet_ntoa(sock.conn_addr.sin_addr), ntohs(sock.conn_addr.sin_port));
-		fflush(stdout);
-	}
-	else if(read_size == -1) {
-		perror("recv failed");
-	}
+	printf("[%s-SERVER]: Client %s:%d disconnected\n", NICK, inet_ntoa(sock.conn_addr.sin_addr), ntohs(sock.conn_addr.sin_port));
 }
 
-void init_client() {
+void init_out_client() {
 	int connected = 0, valid_server_ip = 0;
 	char error_message[MAXBUF], server_ip[MAXBUF];
 	// Initialize IP address / port structure
-	bzero(&self.client_addr, sizeof(self.client_addr));
-	self.client_addr.sin_family = AF_INET;
-	self.client_addr.sin_port = htons(PORT);
+	bzero(&out_client.conn_addr, sizeof(struct sockaddr_in));
+	out_client.conn_addr.sin_family = AF_INET;
+	out_client.conn_addr.sin_port = htons(PORT);
 
 	// Create the socket
-	if((self.client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	if((out_client.socket_desc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		sprintf(error_message, "Error creating socket: %s\n", strerror(errno));
 		print_error_message(error_message);
 	}
@@ -137,13 +129,13 @@ void init_client() {
 	while(!valid_server_ip || !connected) {
 		printf("Enter server IP: ");
 		scanf("%s", server_ip);
-		if(inet_aton(server_ip, &self.client_addr.sin_addr) == 0) {
+		if(inet_aton(server_ip, &out_client.conn_addr.sin_addr) == 0) {
 			valid_server_ip = 0;
 			printf("Error: Not a valid IP\n");
 		}
 		else {
 			valid_server_ip = 1;
-			if(connect(self.client_fd, (struct sockaddr*)&self.client_addr, sizeof(self.client_addr)) == 0) {
+			if(connect(out_client.socket_desc, (struct sockaddr*)&out_client.conn_addr, sizeof(struct sockaddr_in)) == 0) {
 				connected = 1;
 			}
 			else {
@@ -154,15 +146,14 @@ void init_client() {
 	}
 }
 
-void handle_client() {
+void handle_out_client() {
 	char client_buffer[MAXBUF], option[MAXBUF];
-	init_client();
-	printf("[%s-CLIENT]: Connected to server: %s:%d\n", NICK, inet_ntoa(self.client_addr.sin_addr), PORT);
+	init_out_client();
+	printf("[%s-CLIENT]: Connected to server: %s:%d\n", NICK, inet_ntoa(out_client.conn_addr.sin_addr), PORT);
 	while(1) {
 		printf("Enter option bro\n");
 		scanf("%s", option);
 	}
-	close(self.client_fd);
 }
 
 int main(int argc, char *argv[]) {
@@ -171,7 +162,7 @@ int main(int argc, char *argv[]) {
 	printf("Enter nick: ");
 	scanf("%s", NICK);
 	pthread_create(&server_thread, NULL, handle_server, NULL);
-	pthread_create(&client_thread, NULL, handle_client, NULL);
+	pthread_create(&client_thread, NULL, handle_out_client, NULL);
 	pthread_join(server_thread, NULL);
 	pthread_join(client_thread, NULL);
 	return 0;
